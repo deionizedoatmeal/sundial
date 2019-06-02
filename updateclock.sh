167819ed3cd52e505d7f27c82a45128d71a8c9d5
Ian K. Bania <48041892+deionizedoatmeal@users.noreply.github.com>
2019-06-01 20:00:11 -0700

    Update updateclock.sh

--- updateclock.sh
+++ updateclock.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 cd
+
 sudo pkill python
+echo(KILLED)
 
 # remove old version
 sudo rm -r ~/sundial
+echo(REMOVED)
 
 # clone in the new version
 git clone https://github.com/deionizedoatmeal/sundial.git
+echo(CLONED)
 
 # move scripts to bin
 sudo cp ~/sundial/updateclock.sh /bin/updateclock
+echo(MOVED)
 
 # start the clock
